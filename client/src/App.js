96756b33d293c8c1bb4720343245514309eb359e
Ishant <ishantgupta777@gmail.com>
2020-05-03 01:02:40 +0530

    linting added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
