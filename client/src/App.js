2fd90fa22c8a3af37de2ca05cf3d092fcc76cfd2
Ishant <ishantgupta777@gmail.com>
2020-05-02 21:55:02 +0530

    boilerplate done

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
