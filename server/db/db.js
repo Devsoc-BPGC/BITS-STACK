1379cb1452797bd37613db6985c45b4012e880cd
Ishant <ishantgupta777@gmail.com>
2020-05-04 21:41:06 +0530

    docker-compose added but persistent data and hotreloading not working

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,10 @@ mongoose.Promise = global.Promise;
 
 mongoose
   .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/testNames', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(() => {
     console.log('Successfully connected to the MONGODB');
