0974d61804e61ee2b90b3649ae2eafef2dca2f04
Ishant <ishantgupta777@gmail.com>
2020-05-07 08:34:30 +0530

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React From Test Branch
+          Learn React
         </a>
         <TestComponent />
       </header>
