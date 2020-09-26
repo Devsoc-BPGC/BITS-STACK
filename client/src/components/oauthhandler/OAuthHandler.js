5aef7e003524d2bbd30b87e3e6d1521ecdd0d9b9
Mynk-9 <Mynk-9@users.noreply.github.com>
2020-09-26 20:12:45 +0530

    Intitial implementation of the Firebase Auth using email and password
    Signup and login functions check if the email is the BITS mail.

---
+++ client/src/components/oauthhandler/OAuthHandler.js
@@ -0,0 +1,71 @@
+const firebase = require('firebase/app');
+require('firebase/auth');
+
+const firebaseConfig = require('./OAuthFirebaseConfig');
+// this file is not present in the git repo, as it is gitignored
+// to prevent API keys getting into public domain
+// a new gitignore is added to this directory to ignore this file.
+
+firebase.initializeApp(firebaseConfig);
+
+/**
+ * @brief check if the email is bits email
+ * @param {string} email the email to check
+ * @returns true if bits mail, false if not
+ */
+function checkIfBITSMail(email) {
+  const address = email.split('@');
+  return address[1] === 'goa.bits-pilani.ac.in';
+}
+
+/**
+ * @brief signs up the user
+ * @param {string} email email ID of the user
+ * @param {string} password password of the user
+ * @returns null if error, currentUser object if signup and login is success
+ */
+export const signup = async (email, password) => {
+  if (!checkIfBITSMail(email)) return null;
+
+  await firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(e => {
+      console.log(`Error signing up: ${e.code} ${e.message}`);
+    });
+  return firebase.auth().currentUser;
+};
+
+/**
+ * @brief logs in the user
+ * @param {string} email email ID of the user
+ * @param {string} password password of the user
+ * @returns null if error, currentUser object if login is success
+ */
+export const login = async (email, password) => {
+  if (!checkIfBITSMail(email)) return null;
+
+  await firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(e => {
+      console.log(`Error logging in: ${e.code} ${e.message}`);
+    });
+  return firebase.auth().currentUser;
+};
+
+/**
+ * @brief logs out of the current user
+ * @returns true if success, false if failure
+ */
+export const logout = async () => {
+  let success = true;
+  await firebase
+    .auth()
+    .signOut()
+    .catch(e => {
+      success = false;
+      console.log(`Error logging out: ${e.code} ${e.message}`);
+    });
+  return success;
+};
