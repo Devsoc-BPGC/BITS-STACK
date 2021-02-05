18b3a526fadb7e5b00be467692a74b7f78282c39
Devesh <rockdev1234560@gmail.com>
2021-02-06 01:42:13 +0530

    navbar added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
 import './App.css';
-import TestComponent from './components/testComponent';
-import SidebarTop from './components/SideBarTopBar';
+import PostQuestion from './components/postQuestionPage';
+import PrimarySearchAppBar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className='App'>
-<SidebarTop/>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={PrimarySearchAppBar} />
+        <Route exact path="/PostQuestion" component={PostQuestion} />
+      </Switch>
+    </Router>
     </div>
   );
 }
