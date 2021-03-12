2e5d98062d2527efc2607d3f2ab650e0db9e7473
Devesh <rockdev1234560@gmail.com>
2021-03-12 23:54:17 +0530

    forums page ready

---
+++ src/components/dashBoard/Followup/style.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 100,
+        minHeight: 200,
+      },
+      title: {
+        fontSize: 14,
+      },
+      pos: {
+        marginBottom: 12,
+      },
+  });
+
+export default useStyles;
